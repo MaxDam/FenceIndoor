527ae2207ab5caf6474eb7379f7c9ba9585cd869
Massimiliano D'Amico <max@MacBook-Pro-di-Massimiliano.local>
2019-01-27 15:23:38 +0100

    export csv

--- FenceIndoorServer/test.py
+++ FenceIndoorServer/test.py
@@ -4,6 +4,7 @@ import logging
 import commonEngine as com
 import dbEngine as dao
 import annEngine as ann
+import numpy as np
 
 #test del training
 def testTraining():
@@ -13,6 +14,13 @@ def testTraining():
     #addestra l'ann
     ann.buildAndFitAnn(X, Y)
 
+#export db into csv
+def exportDBIntoCsv():
+    X, Y = ann.makeDataFromDb()
+    np.savetxt("./tmp/X.csv", X, delimiter=",",fmt='%.0f')
+    np.savetxt("./tmp/Y.csv", Y, delimiter=",",fmt='%.0f')
+
 #main
 if __name__ == '__main__':
-    testTraining()
+    #testTraining()
+    exportDBIntoCsv()
